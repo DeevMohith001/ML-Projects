3a89773a5c4fbf2329b04d4f4f56be5fd4edb7cf
DeevMohith001 <mohitrajdeev2006@gmail.com>
2026-05-24 22:39:50 +0530

    updated file path

--- Movie Recommender System/Movie_recommender_system/app.py
+++ Movie Recommender System/Movie_recommender_system/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pickle
 
-movies_list = pickle.load(open('"C:/Users/lenovo/Machine Learning Projects/Movie Recommender System/movies.pkl"', 'rb'))
+movies_list = pickle.load(open('C:/Users/lenovo/Machine Learning Projects/Movie Recommender System/movies.pkl', 'rb'))
 movies_list = movies_list['title'].values
 st.title('Movie Recommendation System')
 
