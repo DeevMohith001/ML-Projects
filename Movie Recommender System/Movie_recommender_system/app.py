c685337a1b979beb66534994527c3b4cd11b67c2
DeevMohith001 <mohitrajdeev2006@gmail.com>
2026-05-24 22:03:43 +0530

    Initial commit

---
+++ Movie Recommender System/Movie_recommender_system/app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pickle
+
+movies_list = pickle.load(open('C:/Users/lenovo/Machine Learning Projects/movies.pkl', 'rb'))
+movies_list = movies_list['title'].values
+st.title('Movie Recommendation System')
+
+option = st.selectbox(
+    'How would you like  to contacted?',
+    movies_list
+)
